2cfd8a0c89aa34e20e995240500ba701a0ea82dd
robertbudini <50487061+robertbudini@users.noreply.github.com>
2019-05-11 12:13:53 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,22 @@
 node default {
-}
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
-    file { '/root/README':
+  file {'/root/README':
     ensure => file,
-    content => '$fqdn',
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
   }
 }
-node /^web/ {
-include role::app_server
+node /^web/ { 
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
 }
 
-node /^db/ {
-include role::db_server
-}
